055986376e1938f4f0d67edc68175f9db700bb80
Bobby <bobbyschlicht@gmail.com>
2024-05-30 11:44:35 -0700

    Update xbox_360.cpp

--- src/dac_algorithms/xbox_360.cpp
+++ src/dac_algorithms/xbox_360.cpp
@@ -82,13 +82,11 @@ void actuateLeverlessReport(GpioToButtonSets::F1::ButtonSet buttonSet) {
     bool dUp = buttonSet.up && mod;
     bool dDown = buttonSet.down && mod;
 
-    // R / R + Modifier button -> R / Home
-    bool r = buttonSet.r && (!mod);
-    bool home = buttonSet.r && mod;
-
-    // B / B + Modifier Button -> B / Back
-    bool back = buttonSet.b && mod;
-    bool b = buttonSet.b && !(mod);
+    // R / B + Modifier Button -> Home / View
+    bool home = buttonSet.r && mod;         // Home
+    bool r = buttonSet.r && !(mod);         // R
+    bool back = buttonSet.b && mod;         // View
+    bool b = buttonSet.b && !(mod);         // B
 
     USBConfigurations::Xbox360::ControllerReport &xInputReport = USBConfigurations::Xbox360::xInputReport;
     xInputReport.reportId = 0;
@@ -117,7 +115,7 @@ void actuateLeverlessReport(GpioToButtonSets::F1::ButtonSet buttonSet) {
 };
 
 // Basic-ass DAC Algorithm for Mutliversus. Copied from actuateXbox360Report() so 
-// it doesn't have movement modifiers or proper SOCD yet.
+// it doesn't have movement modifiers or non-neutral SOCD yet.
 void actuateMultiversusReport(GpioToButtonSets::F1::ButtonSet buttonSet) {
     // Modifier button is L
     bool mod = buttonSet.l;
@@ -125,6 +123,12 @@ void actuateMultiversusReport(GpioToButtonSets::F1::ButtonSet buttonSet) {
     // up2 -> up
     // up -> up (might as well)
     buttonSet.up = buttonSet.up || buttonSet.up2;
+
+    // MX / MY + Modifier Button -> L3 / R3
+    bool mx = buttonSet.mx && !(mod);
+    bool my = buttonSet.my && !(mod);
+    bool l3 = buttonSet.mx && mod;
+    bool r3 = buttonSet.my && mod;
 
     // Raw movement on left stick.
     bool left = buttonSet.left && !(mod);
@@ -136,17 +140,25 @@ void actuateMultiversusReport(GpioToButtonSets::F1::ButtonSet buttonSet) {
     uint8_t lx_coord = left && right ? 0 : left ? 0x8000 : right ? 0x7FFF : 0;
     uint8_t ly_coord = down && up ? 0 : down ? 0x8000 : up ? 0x7FFF : 0;
     
+    // Apply Modifier(s) to left stick.
+    // lx_coord = ;
+    // ly_coord = ;
+    
     // Resolve SOCD on right stick (Neutral).
     uint8_t rx_coord = buttonSet.cLeft && buttonSet.cRight ? 0 : buttonSet.cLeft ? 0x8000 : buttonSet.cRight ? 0x7FFF : 0;
     uint8_t ry_coord = buttonSet.cDown && buttonSet.cUp ? 0 : buttonSet.cDown ? 0x8000 : buttonSet.cUp ? 0x7FFF : 0
 
+    // Apply Modifier(s) to right stick.
+    // rx_coord = ;
+    // ry_coord = ;
+	
     // Left stick + Modifer button -> D-Pad
     bool dLeft = buttonSet.left && mod;
     bool dRight = buttonSet.right && mod;
     bool dUp = buttonSet.up && mod;
     bool dDown = buttonSet.down && mod;
-
-    // Start / B + Modifier Button -> Home / View
+	
+    // R / B + Modifier Button -> Home / View
     bool home = buttonSet.r && mod;         // Home
     bool r = buttonSet.r && !(mod);         // R
     bool back = buttonSet.b && mod;         // View
@@ -157,8 +169,8 @@ void actuateMultiversusReport(GpioToButtonSets::F1::ButtonSet buttonSet) {
 
     USBConfigurations::Xbox360::ControllerReport &xInputReport = USBConfigurations::Xbox360::xInputReport;
     xInputReport.reportId = 0;
-    xInputReport.rightStickPress = 0;                       // Inaccessible
-    xInputReport.leftStickPress = 0;                        // Inaccessible
+    xInputReport.rightStickPress = l3;                       // MX + L
+    xInputReport.leftStickPress = r3;                        // MY + L
     xInputReport.back = back;
     xInputReport.start = start;
     xInputReport.dRight = dRight;
@@ -175,17 +187,11 @@ void actuateMultiversusReport(GpioToButtonSets::F1::ButtonSet buttonSet) {
     xInputReport.y = buttonSet.y;
     xInputReport.leftTrigger = buttonSet.ms ? 255 : 0;      // MS = LT
     xInputReport.rightTrigger = r ? 255 : 0
-    // xInputReport.leftStickX = left && right ? 0 : left ? 0x8000 : right ? 0x7FFF : 0;
     xInputReport.leftStickX = lx_coord;
-    // xInputReport.leftStickY = down && up ? 0 : down ? 0x8000 : up ? 0x7FFF : 0;
     xInputReport.leftStickY = ly_coord;
-    // xInputReport.rightStickX = buttonSet.cLeft && buttonSet.cRight ? 0 : buttonSet.cLeft ? 0x8000 : buttonSet.cRight ? 0x7FFF : 0;
     xInputReport.rightStickX = rx_coord;
-    // xInputReport.rightStickY = buttonSet.cDown && buttonSet.cUp ? 0 : buttonSet.cDown ? 0x8000 : buttonSet.cUp ? 0x7FFF : 0;
     xInputReport.rightStickY = ry_coord;
-
 };
-
 
 }
 }
